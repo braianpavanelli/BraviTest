fb0dc1d7eb9e0fec23ce1e9a4afcca757b13d3b1
Braian Pavanelli <braian.pavanelli@consultsolucoes.com.br>
2019-11-06 10:44:37 -0300

    Included Services and App on IOC

--- BraviBraian.Infra.IoC/BootStrapper.cs
+++ BraviBraian.Infra.IoC/BootStrapper.cs
@@ -1,4 +1,8 @@
-﻿using BraviBraian.Domain.Interfaces.Repositories;
+﻿using BraviBraian.Application;
+using BraviBraian.Application.Interface;
+using BraviBraian.Domain.Interfaces.Repositories;
+using BraviBraian.Domain.Interfaces.Services;
+using BraviBraian.Domain.Services;
 using BraviBraian.Infra.Data.Repository;
 using SimpleInjector;
 
@@ -12,6 +16,16 @@ namespace BraviBraian.Infra.IoC
             container.Register<IPersonRepository, PersonRepository>(Lifestyle.Singleton);
             container.Register<IContactRepository, ContactRepository>(Lifestyle.Singleton);
             container.Register<IContactTypeRepository, ContactTypeRepository>(Lifestyle.Singleton);
+
+            container.Register(typeof(IBaseService<>), typeof(BaseService<>), Lifestyle.Scoped);
+            container.Register<IPersonService, PersonService>(Lifestyle.Singleton);
+            container.Register<IContactService, ContactService>(Lifestyle.Singleton);
+            container.Register<IContactTypeService, ContactTypeService>(Lifestyle.Singleton);
+
+            container.Register(typeof(IAppBaseService<>), typeof(AppBaseService<>), Lifestyle.Scoped);
+            container.Register<IPersonAppService, PersonAppService>(Lifestyle.Singleton);
+            container.Register<IContactAppService, ContactAppService>(Lifestyle.Singleton);
+            container.Register<IContactTypeAppService, ContactTypeAppService>(Lifestyle.Singleton);
         }
     }
 }
